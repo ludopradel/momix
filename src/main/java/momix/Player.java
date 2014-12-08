933cacea5bb0ef8c31776c03a6a5018cccdd1aeb
xqjf8651 <lpradel.ext@orange.com>
2014-12-08 17:28:14 +0100

    Player have experience and health points

--- src/main/java/momix/Player.java
+++ src/main/java/momix/Player.java
@@ -1,11 +1,17 @@
 package momix;
 
+
 public class Player {
 
 	private String name;
+	private Integer experience;
+	private Integer health;
+	final int INITIAL_HEALTH_POINTS = 10;
 
 	public Player(String name) {
 		this.name = name;
+		this.experience = 0;
+		health = INITIAL_HEALTH_POINTS;
 	}
 	
 	@Override
@@ -14,6 +20,18 @@ public class Player {
 		
 		Player otherPlayer = (Player) obj;
 		return name.equals(otherPlayer.name);
+	}
+
+	public Integer experience() {
+		return experience;
+	}
+
+	public Integer health() {
+		return health;
+	}
+
+	public void gainExperience(int experience) {
+		this.experience += experience;
 	}
 
 }
--- src/test/java/momix/PlayerShould.java
+++ src/test/java/momix/PlayerShould.java
@@ -1,12 +1,40 @@
 package momix;
 
 import org.junit.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
 
 public class PlayerShould {
 
+	private static final String LUDO = "LUDO";
+	private static final Player A_PLAYER_NAMED_LUDO = new Player(LUDO);
+	private static final int NO_EXPERIENCE = 0;
+	private static final int FULL_HEALTH_BAR = 10;
+
 	@Test public void
 	haveAName() {
-		
+		Player player = new Player(LUDO);
+		assertThat(player).isEqualTo(A_PLAYER_NAMED_LUDO);
+	}
+	
+	@Test public void
+	haveAnEmptyExperienceBar() {
+		Player player = new Player(LUDO);
+		assertThat(player.experience()).isEqualTo(NO_EXPERIENCE);
 	}
 
+	@Test public void
+	haveAFullHealthBar() {
+		Player player = new Player(LUDO);
+		assertThat(player.health()).isEqualTo(FULL_HEALTH_BAR);
+	}
+	
+	
+	@Test public void 
+	gainExperience() {
+		Player player = new Player(LUDO);
+		player.gainExperience(10);
+		
+		assertThat(player.experience()).isEqualTo(10);
+	}
 }
